16fbaedd42f311469b36b1de52c16661dd33784b
y-yamanaka0509 <y_yamanaka@wisenet.co.jp>
2016-09-25 21:07:40 +0900

    Mod Kadai4_1
    課題4_1 変更

--- Kadai4.java
+++ Kadai4.java
@@ -1,21 +1,35 @@
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Kadai4 {
 
 	public static void main(String args[]) {
-		Kadai4_dao dao = new Kadai4_dao();
 
 		Connection con = null;
+		boolean isConnected = false;
 
+		// 接続
 		try {
-			// 接続
-			dao.connectDB(con);
+			con = DriverManager.getConnection(Kadai4_const.SQL_URL, Kadai4_const.SQL_USER, Kadai4_const.SQL_PASS);
+			System.out.println("MySQLへの接続に成功しました。");
+			isConnected=true;
 		} catch (Exception e) {
-			// TODO: handle exception
-		} finally {
-			// クローズ
-			dao.closeDB(con);
+			System.out.println("MySQLへの接続に失敗しました。");
+			System.out.println(e.getMessage());
 		}
+
+		// クローズ
+		if (isConnected) {
+			try {
+				con.close();
+				System.out.println("MySQLのクローズに成功しました。");
+			} catch (SQLException e) {
+				System.out.println("MySQLのクローズに失敗しました。");
+				System.out.println(e.getMessage());
+			}
+		}
+
 	}
 
 }
---
+++ Kadai4_const.java
@@ -0,0 +1,13 @@
+
+public class Kadai4_const {
+
+	/** My SQLのurl */
+	public static final String SQL_URL = "jdbc:mysql://localhost/test";
+
+	/** My SQLのuser */
+	public static final String SQL_USER = "root";
+
+	/** My SQLのpassword */
+	public static final String SQL_PASS = "rsp3swte";
+
+}
