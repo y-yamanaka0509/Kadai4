1633e7f9bc6785426b4bc29f0d2ab2f03d88debe
y-yamanaka0509 <y_yamanaka@wisenet.co.jp>
2016-09-28 13:38:39 +0900

    Mod Kadai4_5
    課題4_5に変更

--- Kadai4.java
+++ Kadai4.java
@@ -1,12 +1,14 @@
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.Scanner;
 
 public class Kadai4 {
 
 	public static void main(String args[]) {
 
 		Kadai4_dao dao = new Kadai4_dao();
+		Scanner sc = new Scanner(System.in);
 		Connection con = null;
 		Statement stm = null;
 		ResultSet rs = null;
@@ -19,11 +21,32 @@ public class Kadai4 {
 			con = dao.connectDB(con);
 			if (con != null) {
 
-				//// SELECT文の設定
-				// dao.setSQLSelect(con,stm);
+				System.out.println("処理を選択してください。（1:SELECT / 2:INSERT / 3:UPDATE / 4:DELETE）");
+				System.out.print("⇒");
 
-				// INSERT文の設定
-				dao.setSQLInsert(con, stm);
+				switch (sc.next()) {
+
+				case "1":
+					// SELECT文の設定
+					dao.setSQLSelect(con, stm);
+					break;
+
+				case "2":
+					// INSERT文の設定
+					dao.setSQLInsert(con, stm);
+					break;
+
+				case "3":
+					// UPDATE文の設定
+					break;
+
+				case "4":
+					// DELETE文の設定
+					break;
+
+				default:
+					System.out.println("処理を終了します。");
+				}
 
 			}
 
@@ -34,6 +57,7 @@ public class Kadai4 {
 		} finally {
 			// クローズ
 			dao.closeDB(con, stm, rs);
+			sc.close();
 		}
 
 	}
